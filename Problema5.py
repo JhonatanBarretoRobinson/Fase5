8da1e74bc59b73ed0a919586ff0dccc66e3c706a
JB <jhonnathanbarreto519@gmail.com>
2026-05-25 14:51:13 -0500

    crear arreglo

---
+++ Problema5.py
@@ -0,0 +1,14 @@
+"""
+Problema 5: Caluclo de horas trabajadas semanales
+Curso: Fundamento de programacion (213022)
+Estudiante: Jhonatan Barreto Robinson
+"""
+
+horas_semana = [
+    ["Ana Perez", 8,8,8,8,8],
+    ["Luis Gomez", 7,8,6,8,7],
+    ["Maria Rodriguez", 8,8,8,7,6],
+    ["Carlos Sanchez", 6,7,8,8,8],
+    ["Sofia Martinez", 9,9,9,9,9]
+]
+
