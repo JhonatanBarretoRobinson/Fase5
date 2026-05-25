59a929eba29bd2b30772f3b35e02a0c73ec3bd6e
JB <jhonnathanbarreto519@gmail.com>
2026-05-25 16:57:21 -0500

    Eliminar 1 usuario del arreglo

--- Problema5.py
+++ Problema5.py
@@ -6,7 +6,6 @@ Estudiante: Jhonatan Barreto Robinson
 
 horas_semana = [
     ["Ana Perez", 8, 8, 8, 8, 8],
-    ["Luis Gomez", 7, 8, 6, 8, 7],
     ["Maria Rodriguez", 8, 8, 8, 7, 6],
     ["Carlos Sanchez", 6, 7, 8, 8, 8],
     ["Sofia Martinez", 9, 9, 9, 9, 9]
