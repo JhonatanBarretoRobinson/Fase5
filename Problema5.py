5a59b698a3896bf946094271b562bf8030695448
JB <jhonnathanbarreto519@gmail.com>
2026-05-25 15:52:11 -0500

    Ajuste  arreglo

--- Problema5.py
+++ Problema5.py
@@ -5,11 +5,11 @@ Estudiante: Jhonatan Barreto Robinson
 """
 
 horas_semana = [
-    ["Ana Perez", 8,8,8,8,8],
-    ["Luis Gomez", 7,8,6,8,7],
-    ["Maria Rodriguez", 8,8,8,7,6],
-    ["Carlos Sanchez", 6,7,8,8,8],
-    ["Sofia Martinez", 9,9,9,9,9]
+    ["Ana Perez", 8, 8, 8, 8, 8],
+    ["Luis Gomez", 7, 8, 6, 8, 7],
+    ["Maria Rodriguez", 8, 8, 8, 7, 6],
+    ["Carlos Sanchez", 6, 7, 8, 8, 8],
+    ["Sofia Martinez", 9, 9, 9, 9, 9]
 ]
 
 UMBRAL_HORAS = 40
